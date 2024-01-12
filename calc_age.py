ee93b0ffe25bc6051e9e8f3a0794761b3267dae8
LaxmiSN <laxmi.rangawale@outlook.com>
2024-01-12 14:14:59 +0530

    commented input code and just kept print statement

--- calc_age.py
+++ calc_age.py
@@ -1,7 +1,10 @@
-import datetime
+# import datetime
+#
+# year = input('YOB: ')
+# today = datetime.date.today()
+# age = today.year  - int(year)
+#
+# print(f"Your age is {age}")
 
-year = input('YOB: ')
-today = datetime.date.today()
-age = today.year  - int(year)
 
-print(f"Your age is {age}")
+print("Hello!!! This is my first Jenkins Job!!!!")
